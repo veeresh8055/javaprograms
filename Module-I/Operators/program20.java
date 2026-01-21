19d54f22db369531f4134face4466f4bc5b284bb
veeresh8055 <visesync@gmail.com>
2026-01-21 18:29:51 +0530

    Operators in Java

---
+++ Module-I/Operators/program20.java
@@ -0,0 +1,43 @@
+public class program20 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 13;
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(a ^ b);
+
+        int x = 25;
+        int y = 21;
+        System.out.println(x & y);
+        System.out.println(x | y);
+        System.out.println(x ^ y);
+
+        int p = 23;
+        int q = 45;
+        System.out.println(p & q);
+        System.out.println(p | q);
+        System.out.println(p ^ q);
+
+         int s = 50;
+        int v = 27;
+        System.out.println(s & v);
+        System.out.println(s | v);
+        System.out.println(s ^ v);
+
+    }
+}
+// output : 
+// 8
+// 15
+// 7
+// 17
+// 29
+// 12
+// 5
+// 63
+// 58
+
+// 18
+// 59
+// 41
