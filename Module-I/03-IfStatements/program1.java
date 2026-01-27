0b1ebf7e07798e1c5784b67d2e532731042e1eb9
veeresh8055 <visesync@gmail.com>
2026-01-27 10:53:18 +0530

    if statements

--- Module-I/03-IfStatements/program1.java
+++ Module-I/03-IfStatements/program1.java
@@ -1,14 +1,19 @@
 public class program1 {
-    public static void main(String[] args) {
-        System.out.println("Program starts ....!");
+  public static void main(String[] args) {
+    System.out.println("Program starts ....!");
 
-          if(true){
-            System.out.println("Java Executed.");
-          }
-          if(false){
-            System.out.println("Program nat execeuted ");
-          }
-
-        System.out.println("Program ends ....!");
+    if (true) {
+      System.out.println("Java Executed.");
     }
+    // if(false){
+    // System.out.println("Program nat execeuted ");
+    // }
+
+    int a = 4;
+    if (a < 8) {
+      System.out.println("a is greater than 8 ");
+    }
+
+    System.out.println("Program ends ....!");
+  }
 }
